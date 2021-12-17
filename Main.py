e0ed2bbeb11ae6d12161dd6ad880e47d863b6066
kaanlab <kaan@live.ru>
2021-12-17 10:39:05 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,12 +6,11 @@ password = str(input())
 keys = ['zAg', 'gb', 'N34', 'h@90', 'vql', 'N', 'Z', 'b', 'BsqY', 'ZcX', 'nBn', '1f5H4', 'sCrg', 'Gb', 'CyA', '#$fsq', 'Ho4', 'o!2s', 'z$h', 'am*', 'WgB', 'llkl', '<<jk', 'meMo', 'q|', 'Gm', 'sFgsa', 'oV', '^', 'vN', 'in', 'sId', 'LoV', 'nAt', 'GMB', 'f@1', 'z%6', '|s|', 'wEq']
 
 output = ''
-seeds = [ord(i) * m.sqrt(r.randint(100, 500)) for i in set(password)]
+seeds = [ord(i) * m.pow(r.randint(100, 500), len(password)) for i in list(password)]
 
 for i in seeds:
     r.seed(i)
     if i > len(set(password)):
-        output += r.choice(list(set(password)))
         output += str(r.randint(20, 100))
         output += r.choice(keys)
         r.shuffle(keys)
@@ -23,10 +22,9 @@ for i in seeds:
 
 print(output)
 
-#123-password : d72CyAr46h@90a95sIdo41q|w90z%6r77nAtw51ino54N
-#123-wordpass : o95gbr76z%6p45Gbs50wEqd100h@90d64wEqa96CyAo32sFgsa
-#123-pass : a72CyAa72WgBp95Gma41sCrg
+#123-password : 84b22b37Gb54llkl57llkl50h@9050am*68N34
+#123-wordpass : 43llkl78sFgsa91#$fsq21^79#$fsq95nAt73ZcX24<<jk
+#123-pass :29in60sId30nAt96N34
+#123-passwork : 84b22b37Gb54llkl57llkl50h@9050am*91z$h
 
-#123-password : r77sIdr63q|d99nBnw41h@90d33q|r831f5H4d51Gm
-#123-wordpass : p72CyAr38meMoa95Zd50vqlp62gbw64q|p66LoV
-#123-pass : a100sFgsas72vNa82f@1
+#great dependance of passwords' length...
